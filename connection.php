146e21354a4cb7429aff21a35e59c6130e215ee8
Aishwarya1112003 <123392365+Aishwarya1112003@users.noreply.github.com>
2024-11-20 01:25:12 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+
+
+<?php
+//change mysqli_connect(host_name,username, password); 
+$connection = mysqli_connect("localhost", "root", "Ram1234*");
+$db = mysqli_select_db($connection, 'foodwaste_db');
+?>
